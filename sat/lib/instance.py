da25d9908eec8bff5996bb0d89dbb2570e9b432f
Tomas Patro <tomas.patro@gmail.com>
2020-12-29 21:20:09 +0100

    Add initial sat structure

---
+++ sat/lib/instance.py
@@ -0,0 +1,6 @@
+class Instance():
+    def __init__(self, var_num, cls_num, weights, clauses):
+        self.var_num = var_num
+        self.cls_num = cls_num
+        self.weights = weights
+        self.clauses = clauses
---
+++ sat/lib/solution.py
@@ -0,0 +1,6 @@
+class Solution():
+    def __init__(self, var_num):
+        self.w_sum = 0
+        self.time = 0.0
+        self.rel_err = 0.0
+        self.conf = [False] * var_num
